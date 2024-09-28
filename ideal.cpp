be349f99cd0c7ce911a5d6130398ce69f3524cc6
eg-mart <marteg71@gmail.com>
2024-09-28 10:38:08 +0300

    add input error checking

--- ideal.cpp
+++ ideal.cpp
@@ -9,12 +9,26 @@ int main()
 {
     size_t size;
     std::cin >> size;
+    if (!std::cin.good()) {
+        std::cerr << "An I/O error has occured on size input" << std::endl;
+        return 1;
+    }
 
     size_t test_count;
     std::cin >> test_count;
+    if (!std::cin.good()) {
+        std::cerr << "An I/O error has occured on test_count input" << std::endl;
+        return 1;
+    }
+
     std::vector<int> tests(test_count);
-    for (int i = 0; i < test_count; i++)
+    for (int i = 0; i < test_count; i++) {
         std::cin >> tests[i];
+        if (!std::cin.good()) {
+            std::cerr << "An I/O error has occured on tests input" << std::endl;
+            return 1;
+        }
+    }
 
     auto cache = cachelib::IdealCache<int, int>(size, tests.begin(), tests.end());
 
--- lirs.cpp
+++ lirs.cpp
@@ -10,15 +10,23 @@
 int main()
 {
     size_t size, lir_size, hir_size;
+    std::cin >> size;
+    if (!std::cin.good()) {
+        std::cerr << "An I/O error has occured on size input" << std::endl;
+        return 1;
+    }
 
 #ifdef SET_HIRS_SIZE
-    std::cin >> size >> hir_size;
-    lir_size = std::max<size_t>(size - hir_size, 1);
+    std::cin >> hir_size;
+    if (!std::cin.good()) {
+        std::cerr << "An I/O error has occured on hir_size input" << std::endl;
+        return 1;
+    }
 #else
-    std::cin >> size;
     hir_size = std::floor(std::cbrt(size));
-    lir_size = std::max<size_t>(size - hir_size, 1);
 #endif // SET_HIRS_SIZE
+
+    lir_size = std::max<size_t>(size - hir_size, 1);
 
     if (hir_size < 1 || lir_size < 1) {
         std::cerr << "Error: size must be greater than 1" << std::endl;
@@ -27,9 +35,18 @@ int main()
 
     size_t test_count = 0;
     std::cin >> test_count;
+    if (!std::cin.good()) {
+        std::cerr << "An I/O error has occured on test_count input" << std::endl;
+        return 1;
+    }
+
     std::vector<int> tests(test_count); 
     for (size_t i = 0; i < test_count; i++) {
         std::cin >> tests[i];
+        if (!std::cin.good()) {
+            std::cerr << "An I/O error has occured on tests input" << std::endl;
+            return 1;
+        }
     }
 
     auto cache = cachelib::LIRSCache<int, int>(lir_size, hir_size);
